4b16d7c7694eab37eff172c1d5aa279ef0d68403
csabourin <christian.sabourin@canada.ca>
2019-10-07 07:50:19 -0400

    Build a Work plan update

--- pages/buildWP.vue
+++ pages/buildWP.vue
@@ -1,64 +1,78 @@
 <template>
   <div>
     <hamburger />
-    <h1 class="pageTitle">{{$t('BuildWorkPlan')}}</h1>
+    <h1 class="pageTitle">
+      {{ $t('BuildWorkPlan') }}
+    </h1>
     <section>
-      <video ref="videoplayer" width="800" height="600" poster="/_nuxt/assets/sintel.jpg" src="/_nuxt/assets/Slide_2_-_Gov_priorities.mp4" controls v-on:timeupdate="update">
-        <track :src="vttUrl" kind="chapters" default="" v-on:load="generate">
+      <video
+        ref="videoplayer"
+        width="800"
+        height="600"
+        poster="/_nuxt/assets/sintel.jpg"
+        src="/_nuxt/assets/Slide_2_-_Gov_priorities.mp4"
+        controls
+        @timeupdate="update"
+      >
+        <track :src="vttUrl" kind="chapters" default="" @load="generate">
       </video>
-      <div id="bar" ref="linkBar" v-if="1">
+      <div id="bar" ref="linkBar">
+        <span v-for="(item,index) in navBarTracks" :key="index" class="chaptersLink" :data-start="startTime[index]" @click="seek">
+          {{ item }}
+        </span>
       </div>
     </section>
     <div class="bottomNav planSection">
-      <microlearning path="planKey" size="small" completion=100>{{$t('KeyMessages')}}</microlearning>
-      <microlearning path="buildWP" size="small" time="20" completion=80>{{$t('BuildWorkPlan')}}</microlearning>
-      <microlearning size="small" time="20" completion=10>{{$t('CreateBudget')}}</microlearning>
-      <microlearning size="small" time="15">Test</microlearning>
+      <microlearning path="planKey" size="small" completion="100">
+        {{ $t('KeyMessages') }}
+      </microlearning>
+      <microlearning path="buildWP" size="small" time="20" completion="80">
+        {{ $t('BuildWorkPlan') }}
+      </microlearning>
+      <microlearning size="small" time="20" completion="10">
+        {{ $t('CreateBudget') }}
+      </microlearning>
+      <microlearning size="small" time="15">
+        Test
+      </microlearning>
     </div>
   </div>
 </template>
 <script type="text/javascript">
-import vttUrl from "~/assets/chapters.vtt"
-import hamburger from "~/components/hamburger";
-import microlearning from "~/components/microlearning";
-import Vue from 'vue';
+import vttUrl from '~/assets/chapters.vtt'
+import hamburger from '~/components/hamburger'
+import microlearning from '~/components/microlearning'
 export default {
-  data() {
+  data () {
     return {
-      vttUrl
+      vttUrl,
+      startTime: [],
+      endTime: [],
+      navBarTracks: []
     }
-  },
-  computed: {
-    // trackCues(){return this.$refs.videoplayer.textTracks[0].cues;}
   },
   components: {
     hamburger,
     microlearning
   },
   methods: {
-    seek(e) {
-      this.$refs.videoplayer.currentTime = e.target.getAttribute('data-start');
-      console.log(e);
-      if (this.$refs.videoplayer.paused) { this.$refs.videoplayer.play(); }
+    seek (e) {
+      this.$refs.videoplayer.currentTime = e.target.getAttribute('data-start')
+      if (this.$refs.videoplayer.paused) { this.$refs.videoplayer.play() }
     },
-    generate() {
-      var c = this.$refs.videoplayer.textTracks[0].cues;
+    generate () {
+      const c = this.$refs.videoplayer.textTracks[0].cues
       for (let i = 0; i < c.length; i++) {
-        let s = document.createElement("span");
-        s.innerHTML = c[i].text;
-        s.setAttribute('class', 'chaptersLink')
-        s.setAttribute('data-start', c[i].startTime);
-        s.setAttribute('data-end', c[i].endTime);
-        s.setAttribute('v-on:click', "seek");
-        s.setAttribute(':key', i);
-        s.style.width = '150px';
-        this.$refs.linkBar.appendChild(s);
+        this.navBarTracks.push(c[i].text)
+        this.startTime[i] = c[i].startTime
+        this.endTime[i] = c[i].endTime
       }
-
     },
-    update(e) {
-      var p = e.target.currentTime / e.target.duration * 100;
-      console.log(p);
+    update (e) {
+      // const p = e.target.currentTime / e.target.duration * 100
+    },
+    checkpoint (x) {
+
     }
   }
 }
@@ -74,7 +88,6 @@ video {
   background: #000;
   cursor: pointer;
 }
-
 
 #bar {
   display: flex;
@@ -98,6 +111,7 @@ video {
   background-color: #ebebeb;
   margin: 5px;
   font-weight: bolder;
+  width: 150px;
 }
 
 .chaptersLink:hover {

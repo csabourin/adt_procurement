2ffe3fd46759efdcb3933fb498bb77676cc710e5
csabourin <christian.sabourin@canada.ca>
2019-10-25 22:33:05 -0400

    doubled currenttime

--- pages/buildWP.vue
+++ pages/buildWP.vue
@@ -131,16 +131,17 @@ export default {
       this.accessiblePopup = false
       this.justSeeked = true
       const videoPlayer = this.$refs.videoplayer
+      const timeData = e.target.getAttribute('data-start')
       videoPlayer.pause()
-      this.isPlayingSoon = e.target.getAttribute('data-start')
-      if(this.isItReady) videoPlayer.currentTime = e.target.getAttribute('data-start')
+      this.isPlayingSoon = timeData
+      videoPlayer.currentTime = timeData
       
       this.isPlayingNow = videoPlayer.currentTime
       const isNow = this.isPlayingNow
       this.currentFrame = this.startTime.findIndex(element => element === isNow)
       localStorage.setItem("WPCurrentPlaying", this.currentFrame)
       this.$nextTick(function () {
-        setTimeout(function() { videoPlayer.play(); }, 250)
+        setTimeout(function() { videoPlayer.play();videoPlayer.currentTime = timeData }, 250)
         this.justSeeked=false
       })
     },

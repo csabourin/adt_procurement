3f3c4db852ef2f04e6ebc570e74d00f8f6a66260
cspsThierryDumas <57180119+cspsThierryDumas@users.noreply.github.com>
2019-11-06 11:11:45 -0500

    Created link files
    Generated files, next task > populate with links from G110[5] and sort using paper guide

---
+++ components/spend_links.vue
@@ -0,0 +1,18 @@
+<template>
+	<span>
+		<div v-if="1i18n.locale=='fr'">
+			<h2>Liens externes et descriptions</h2>
+			<dl>
+				<dt><a href="" target="_blank"></a></dt>
+				<dd></dd>
+			</dl>
+		</div>
+		<div v-if="1i18n.locale=='en'">
+			<h2>External Links and Descriptions</h2>
+			<dl>
+				<dt><a href="" target="_blank"></a></dt>
+				<dd></dd>
+			</dl>	
+		</div>
+	</span>
+</template>

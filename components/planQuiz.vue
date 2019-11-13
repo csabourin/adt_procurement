426ae43f45e06c2df7f09c936adfb9ad1193b811
csabourin <christian.sabourin@canada.ca>
2019-11-13 07:00:57 -0500

    Update planQuiz.vue

--- components/planQuiz.vue
+++ components/planQuiz.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <p><strong>Question 1</strong></p>
-    <p v-html="$t('q1')"></p>
+    <p v-html="$t('q1.text')"></p>
     <select v-model="options1">
-    	<option disabled value=''>{{$t('qDisabled')}}</option>
+      <option disabled value=''>{{$t('qDisabled')}}</option>
       <option v-for="(statement,index) in answers1" value="index">{{$t(statement)}}</option>
     </select>
   </div>
 </template>
 <i18n>
   {
-  "en":{"q1":"Match the following statements to the appropriate sections of the work plan. Each section can only be populated with one statement.",
+  "en":{
   "qDisabled":"Please select one",
+  "q1"{"text":"Match the following statements to the appropriate sections of the work plan. Each section can only be populated with one statement.",
   "a11": "High",
   "a12": "Work with Canadians in all areas of the economy and all parts of the country to improve conditions for investment",
   "a13" : "Tablets",
@@ -23,10 +24,13 @@
   "a19" : "Low",
   "a110" : "Workplace Modernization",
   "a111" : "Give employees tablets" }
+  }
   ,
-  "fr":{"q1":"Faites correspondre les &eacute;nonc&eacute;s suivants aux sections appropri&eacute;es du plan de travail. Chaque section ne peut contenir qu'un seul &eacute;nonc&eacute;.",
-"qDisabled":"Veuillez faire un choix",
-"a11": "Élevé ",
+  "fr":{
+  "qDisabled":"Veuillez faire un choix",
+  "q1"{
+  "text":"Faites correspondre les &eacute;nonc&eacute;s suivants aux sections appropri&eacute;es du plan de travail. Chaque section ne peut contenir qu'un seul &eacute;nonc&eacute;.",
+  "a11": "Élevé ",
   "a12": "Travailler avec les Canadiens dans tous les secteurs de l'économie et dans toutes les régions du pays pour améliorer les conditions d'investissement.",
   "a13" : "Tablettes",
   "a14" : "Conservez les ordinateurs d'origine jusqu'à ce que les tablettes soient prêtes et à l'emploi pendant 3 mois. ",
@@ -37,25 +41,26 @@
   "a19" : "Faible",
   "a110" : "Modernisation du milieu de travail ",
   "a111" : "Donner des tablettes aux employés"
-}
+  }
+  }
   }
 </i18n>
 <script type="text/javascript">
 export default {
   data() {
     return {
-      answers1: ['a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18', 'a19', 'a110', 'a111', ],
-      options1:'',
-      options2:'',
-      options3:'',
-      options4:'',
-      options5:'',
-      options6:'',
-      options7:'',
-      options8:'',
-      options9:'',
-      options10:'',
-      options11:''
+      answers1: ['q1.a11', 'q1.a12', 'q1.a13', 'q1.a14', 'q1.a15', 'q1.a16', 'q1.a17', 'q1.a18', 'q1.a19', 'q1.a110', 'a111', ],
+      options1: '',
+      options2: '',
+      options3: '',
+      options4: '',
+      options5: '',
+      options6: '',
+      options7: '',
+      options8: '',
+      options9: '',
+      options10: '',
+      options11: ''
 
     }
   }

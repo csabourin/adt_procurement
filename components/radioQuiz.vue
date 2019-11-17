3fa884e4827d09b545b3461d1c5c2a827a3dedc8
csabourin <christian.sabourin@canada.ca>
2019-11-16 21:42:53 -0500

    Test component

---
+++ components/radioQuiz.vue
@@ -0,0 +1,32 @@
+<template>
+	<span>
+		<strong v-html="Question.text" />
+    <ul class="list-unstyled">
+      <li v-for="(item,index) in Question.options"><input type="radio" v-model="Quest1" :name="'q'+QNumber" :id="'radioq'+QNumber+index" :value="index"> <label :for="'radioq'+QNumber+index">{{item}}</label></li>
+    </ul>
+    <p v-if="Quest1" v-html="Question.feedback[Quest1]" />
+	</span>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				Quest1:''
+			}
+		},
+		props:{
+			Question:{
+				type:Object,
+				default:{
+					text:"Question",
+					options:{"1":"Option"},
+					feedback:{"1":"Feedback"}
+				}
+			},
+			QNumber:{
+				type:String,
+				default:"0"
+			}
+		}
+	}
+</script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,24 +1,53 @@
 <template>
   <span>
-    <ul>
-      <li v-for="(item,index) in $t('q1')" >{{ index +"<"+ item}}</li>
-    </ul>
-  </span>
-</template>
+    <radioQuiz :Question="$t('q1')" Number="1" />
+  </span> </template>
+  <script type="text/javascript">
+    import radioQuiz from "~/components/radioQuiz"
+    export default {
+      components:{
+        radioQuiz
+      }
+    }
+  </script>
 <i18n>
   {
-  "en":{
-  "q1":{
-  "1":"One",
-  "2":"Two",
-  "3":"Three"
+  "en":
+  {
+  "q1":
+  {
+  "text":"What number comes second?",
+  "options":
+  {
+  "1": "One",
+  "2": "Two",
+  "3": "Three"
+  },
+  "feedback":
+  {
+  "1": "<span class='v-wrong' /> <strong>Incorrect.</strong>",
+  "2": "<span class='v-right' /> <strong>Correct!</strong>",
+  "3": "<span class='v-wrong' /> <strong>Incorrect.</strong>"
+  }
   }
   },
-  "fr":{
-  "q1":{
-  "1":"Un",
-  "2":"Deux",
-  "3":"Trois"
+  "fr":
+  {
+  "q1":
+  {
+  "text":"Quel numéro arrive en deuxième?",
+  "options":
+  {
+  "1": "Un",
+  "2": "Deux",
+  "3": "Trois"
+  },
+  "feedback":
+  {
+  "1": "<span class='v-wrong' /> <strong>Incorrect.</strong>",
+  "2": "<span class='v-right' /> <strong>Correct!</strong>",
+  "3": "<span class='v-wrong' /> <strong>Incorrect.</strong>"
+  }
   }
   }
   }

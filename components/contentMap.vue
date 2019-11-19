5ac39170da8b7cae4af44cbe04b842298268c596
cspsThierryDumas <57180119+cspsThierryDumas@users.noreply.github.com>
2019-11-19 11:04:19 -0500

    Menu redesign (contenMap.vue + mini-change in default.vue) iteration #1
    Still some work to be done + testing but so far it's prettier. Sass did not work so pure CSS for now! Only tested in Chrome.

--- components/contentMap.vue
+++ components/contentMap.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="contentMap">
-    <nuxt-link :to="localePath('index')">
+    <!-- <nuxt-link :to="localePath('index')">
       <h3>{{$t('homePage')}}</h3>
-    </nuxt-link>
+    </nuxt-link> -->
     <h4>{{$t('plan')}}</h4>
     <ul style="list-style: none">
       <li>
@@ -55,13 +55,42 @@
 </template>
 <style type="text/css" scoped>
 .contentMap {
-  background-color: #d8d7d7;
+  background-color: #fff;
   box-shadow: 4px 4px 8px #888;
-  padding: 0.5em
+  padding: 0;
+
+  height:100%;
+  color:#4d4d4d;
 }
 
-a {
+.contentMap a {
   color: #000;
+  width:100%;
+  display: inline-block;
+  padding:4px 16px;
+  border-left-width-width:0px;
+  border-left-style: solid;
+  border-left-color:transparent;
 }
+.contentMap a.nuxt-link-exact-active {
+  background-color:#f0f0f0;
+  border-left-width:4px;
+  padding:4px 16px 4px 15px;
+}
+.contentMap ul:nth-of-type(1) a.nuxt-link-exact-active {border-left-color:#587C84;}
+.contentMap ul:nth-of-type(2) a.nuxt-link-exact-active {border-left-color:#7D677D;}
+.contentMap ul:nth-of-type(3) a.nuxt-link-exact-active {border-left-color:#865F56;}
+
+.contentMap ul {
+    padding:0;
+}
+.contentMap h4{
+  text-align:center;
+  padding:10px 0 10px;
+  text-transform: uppercase;
+}
+.contentMap h4:nth-of-type(1){border-bottom:4px solid #b4c6ca;}
+.contentMap h4:nth-of-type(2){border-bottom:4px solid #d3cad2;}
+.contentMap h4:nth-of-type(3){border-bottom:4px solid #d6c7c3;}
 
 </style>

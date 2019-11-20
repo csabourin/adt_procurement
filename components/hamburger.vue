fe8dba3ea335d78cb65147d319e82b665a0e2be3
csabourin <christian.sabourin@canada.ca>
2019-11-19 21:31:44 -0500

    Update hamburger.vue

--- components/hamburger.vue
+++ components/hamburger.vue
@@ -1,25 +1,54 @@
 <template>
-    <a href="javascript:" @click="ShowMenu" class="hamburger">
-      <img src="~assets/hamburger.png" >
+    <a href="javascript:" @click="ShowMenu" :class="[{'is-active':MenuShowing},'ui-hamburger-02']">
+      &nbsp;&nbsp;&nbsp;&nbsp;
     </a>
 </template>
 <style type="text/css" scoped>
-h1 {
-  margin: .5em;
+.ui-hamburger-02 {
+  top:.5em;
+  width: 50px;
+  height: 26px;
+  border: none;
+  position: relative;
+  background: linear-gradient(to bottom, #4D4D4D, #4D4D4D);
+  background-size: 100% 20%;
+  background-repeat: no-repeat;
+  background-position: center center;
+  transition: background-size 0.2s 0.25s ease;
+  cursor: pointer;
+  text-decoration: none;
 }
-a{
-  color:#35495e;
+.ui-hamburger-02:before,
+.ui-hamburger-02:after {
+  height: 20%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  background-color: #4D4D4D;
+  content: '';
+  transition: transform 0.3s ease;
+}
+.ui-hamburger-02:before {
+  top: 0;
+  transform-origin: 91% 150%;
+}
+.ui-hamburger-02:after {
+  bottom: 0;
+  transform-origin: 91% -50%;
+}
+.ui-hamburger-02.is-active {
+  background-size: 0 20%;
+  transition-delay: 0s;
+}
+.ui-hamburger-02.is-active:before {
+  transform: rotate(-45deg);
+  transition-delay: 0.15s;
+}
+.ui-hamburger-02.is-active:after {
+  transform: rotate(45deg);
+  transition-delay: 0.15s;
 }
 
-.hamburger {
-  background-color: transparent;
-  border-color: transparent;
-}
-
-button.close{
-	border: 1px solid gray;
-	border-radius: 50px;
-}
 </style>
 <script type="text/javascript">
 export default {
@@ -30,6 +59,7 @@ export default {
   },
   methods:{
     ShowMenu(){
+      this.MenuShowing=!this.MenuShowing
       this.$emit('menu-toggle')
     }
    

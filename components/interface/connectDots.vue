6da008d83633be1cf2fbc016c8aa91aefad5fc11
csabourin <christian.sabourin@canada.ca>
2019-12-07 21:19:05 -0500

    Update connectDots.vue

--- components/interface/connectDots.vue
+++ components/interface/connectDots.vue
@@ -1,56 +1,80 @@
 <template>
   <div>
-  	<p>{{svgPosx}} , {{svgPosy}}</p>
+    <p>{{svgPosx}} , {{svgPosy}}</p>
     <ul style="float:left;list-style:none">
-      <li v-for="(item,index) in 5">
-        <input type="radio" :name="'left'+index" :id="'left'+index" :value="index" @click="findLeft">
+      <li v-for="(item,index) in $t('dotsRight')">
+        <input @focus="whereSvg" type="radio" name="left" :id="'left'+index" :value="index" @click="findLeft" v-model="activePath">
         <label :for="'left'+index">{{item}}</label>
       </li>
     </ul>
-    <svg  @load="whereSvg" ref="refSVG" style="float:left" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid slice" viewBox="0 0 180 180" width="180" height="180">
-      <path :d="'M'+left.x+','+left.y+' '+right.x+','+right.y" stroke-width="10" stroke="#000" fill="" />
+    <span >
+    <svg  :key="$i18n.locale" ref="refSVG" style="float:left" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid slice" viewBox="0 0 180 180" width="180" height="180">
+      <path ref="svgPath" v-for="(item, index) in 5" :d="'M'+left.x+','+left.y+' '+right.x+','+right.y" stroke-width="10" stroke="#4d4d4d" fill="" />
     </svg>
+    </span>
     <ul style="float:left;list-style:none">
-      <li v-for="(item,index) in 5"><input type="radio" @click="findRight" ref="thatis" :name="'right'+index" :id="'name2'+index" :value="index"><label :for="'name2'+index">{{item}}</label></li>
+      <li v-for="(item,index) in 5"><input @focus="whereSvg" type="radio" @click="findRight" ref="thatis" name="right" :id="'name2'+index" :value="index"><label :for="'name2'+index">{{item}}</label></li>
     </ul>
+    <br style="clear:both">
     <p>Left {{left.x}} {{left.y}}</p>
     <p>Right {{right.x}} {{right.y}}</p>
+    <p>Active {{activePath}}</p>
   </div>
 </template>
 <script type="text/javascript">
 export default {
   data() {
     return {
-      pathArray: [],
+      activePath: 0,
       left: { x: 0, y: 0 },
       right: { x: 0, y: 0 },
-      svgPosx:0,
-      svgPosy:0
+      svgPosx: 0,
+      svgPosy: 0
     }
   },
 
   methods: {
-    findRight(event) {      
+    findRight(event) {
       const left = event.target.getBoundingClientRect().left
       const top = event.target.getBoundingClientRect().top
-      this.right.x = left- this.svgPosx
-      this.right.y = top- this.svgPosy
+      this.right.x = left - this.svgPosx
+      this.right.y = top - this.svgPosy+16
     },
-  findLeft(event) {
-      
-      const left = event.target.getBoundingClientRect().left
-      const top = event.target.getBoundingClientRect().top
-      this.left.x = left- this.svgPosx
-      this.left.y = top -this.svgPosy
-    },
-    whereSvg(){
-    	this.svgPosx = this.$refs.refSVG.getBoundingClientRect().left 
-      this.svgPosy = this.$refs.refSVG.getBoundingClientRect().top
+    findLeft(event) {
 
+      const left = event.target.getBoundingClientRect().left
+      const top = event.target.getBoundingClientRect().top
+      this.left.x = left - this.svgPosx
+      this.left.y = top - this.svgPosy+16
+    },
+    whereSvg() {
+      this.$nextTick(() => {
+        this.svgPosx = this.$refs.refSVG.getBoundingClientRect().left
+        this.svgPosy = this.$refs.refSVG.getBoundingClientRect().top
+      })
     }
-  },
-  mount(){
-
   }
 }
 </script>
+<i18n>
+	{
+		"en":{
+		"dotsRight":[
+		"One",
+		"Two",
+		"Three",
+		"Four",
+		"Five"
+		]
+	},
+		"fr":{
+		"dotsRight":[
+		"Un",
+		"Deux",
+		"Trois",
+		"Quatre",
+		"Cinq"
+		]
+	}
+	}
+</i18n>

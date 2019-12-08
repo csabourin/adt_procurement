c8138236e793759ee77ff7c5d10edb5fadd1ca2e
csabourin <christian.sabourin@canada.ca>
2019-12-08 14:22:10 -0500

    Update connectDots.vue

--- components/interface/connectDots.vue
+++ components/interface/connectDots.vue
@@ -9,7 +9,7 @@
     </ul>
     <span >
     <svg :key="$i18n.locale" ref="refSVG" style="float:left" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid slice" viewBox="0 0 180 180" width="180" height="180">
-      <path ref="svgPath" v-for="(item, index) in 5" :d="'M'+left.x+','+left.y+' '+right.x+','+right.y" stroke-width="10" stroke="#4d4d4d" fill="" />
+      <path ref="svgPath" v-for="(item, index) in 5" :d="'M'+left.x+','+left.y+' '+right.x+','+right.y" stroke-width="8" stroke="#4d4d4d" fill="" stroke-linecap='round' />
     </svg>
     </span>
     <ul style="float:left;list-style:none">
@@ -35,16 +35,16 @@ export default {
 
   methods: {
     findRight(event) {
-      const left = event.target.getBoundingClientRect().left
-      const top = event.target.getBoundingClientRect().top
-      this.right.x = left - this.svgPosx
+      const left = event.target.parentNode.getBoundingClientRect().left
+      const top = event.target.parentNode.getBoundingClientRect().top
+      this.right.x = left - this.svgPosx-45
       this.right.y = top - this.svgPosy+16
     },
     findLeft(event) {
 
-      const left = event.target.getBoundingClientRect().left
-      const top = event.target.getBoundingClientRect().top
-      this.left.x = left - this.svgPosx
+      const left = event.target.parentNode.getBoundingClientRect().right
+      const top = event.target.parentNode.getBoundingClientRect().top
+      this.left.x = left - this.svgPosx+20
       this.left.y = top - this.svgPosy+16
     },
     whereSvg() {
@@ -77,4 +77,4 @@ export default {
 		]
 	}
 	}
-</i18n>
+</i18n></template>

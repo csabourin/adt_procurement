0823634a70a8ea6bcb67b65d216a84a5350195ef
csabourin <christian.sabourin@canada.ca>
2020-01-03 13:25:26 -0500

    VideoPlayer Updates

--- components/interface/videoPlayer.vue
+++ components/interface/videoPlayer.vue
@@ -1,56 +1,157 @@
 <template>
   <b-row>
-  	<b-col>
-	<script src="https://kit.fontawesome.com/e5ee1a6fb9.js" crossorigin="anonymous"></script>
-    <figure style="clear:both">
-      <video></video>
-    </figure>
-    <div id="video-controls" class="controls" data-state="hidden">
-        <progress id="progress" value="0" min="0">
-          <span id="progress-bar"></span>
-        </progress>
-      <button id="playpause" @click="isPaused=!isPaused" type="button" data-state="play" :aria-label="'Play'" title="Play"><i class="fas fa-play"></i></button>
-      <button id="stop" type="button" aria-label="Stop" title="Stop" data-state="stop"><i class="fas fa-stop"></i></button>
-      <button ref="mute" @click="isMuted=!isMuted" type="button" data-state="mute" :title="'Volume: '+setVolume+'%'"><i :class="{'fas fa-volume-mute':isMuted,'fas fa-volume-up':!isMuted}" /></button><input type="range" v-model="setVolume">
-      <button type="button" data-state="go-fullscreen"><i class="fas fa-compress"></i></button>
-      
-    </div>
-</b-col>
+    <b-col>
+      <script src="https://kit.fontawesome.com/e5ee1a6fb9.js" crossorigin="anonymous"></script>
+      <figure style="clear:both;position:relative;">
+        <video ref="videoplayer" :src="videoUrl" :poster="require('~/assets/'+ $i18n.locale +'/video_poster.PNG')" @timeupdate="lookPlayTime">
+          <track v-if="chapterFile" kind="chapters" :src="chapterFile">
+        </video>
+        <transition name="expand">
+          <div class="CC" v-if="CCactive"></div>
+        </transition>
+        <div ref="video-controls" class="controls" data-state="hidden">
+          <progress @click="setTime" ref="progress" :value="PlayTime" min="0" max="100">
+            <span ref="progress-bar" :style="'width:'+PlayTime+'%'"></span>
+          </progress>
+          <button ref="playpause" @click="setPlaying" type="button" data-state="play" :aria-label="isPaused?$t('play'):$t('pause')" :title="isPaused?$t('play'):$t('pause')"><i :class="{'fas fa-play':isPaused,'fas fa-pause':!isPaused}"></i></button>
+          <button ref="mute" @click="isMuted=!isMuted" type="button" data-state="mute" :title="'Volume: '+setVolume+'%'"><i :class="{'fas fa-volume-mute':isMuted,'fas fa-volume-up':!isMuted}" /></button>
+          <input type="range" v-model="setVolume" :title="'Volume: '+setVolume+'%'" :aria-label="'Volume: '+setVolume+'%'">
+          <!-- <button type="button" data-state="go-fullscreen"><i class="fas fa-compress"></i></button> -->
+          <button @click="showCC" style="float:right" type="button" :title="$t('closedcaptionning')" :aria-label="$t('closedcaptionning')"><i class="fas fa-closed-captioning"></i></button>
+        </div>
+      </figure>
+    </b-col>
   </b-row>
 </template>
 <script type="text/javascript">
-	export default{
-		data(){
-			return{
-				setVolume:100,
-				oldVolume:100,
-				isMuted:false,
-				isPaused:true
-			}
-		},
-		watch:{
-			isMuted(){
-				if(this.isMuted){
-					this.oldVolume=this.setVolume
-					this.setVolume=0}
-					else {
-						this.setVolume=this.oldVolume
-					}
-			},
-			setVolume(volume){
-				if(volume==0){this.isMuted=true} else {
-					this.isMuted=false
-					this.oldVolume=this.setVolume
-				}
-			}
-		}
-	}
-</script>
-<style type="text/css" scoped>
-* {
-  box-sizing: border-box;
+export default {
+  props: {
+    videoFile: { type: String, default: 'IntroVideoPrototype.mp4' },
+    posterFile: { type: String, default: 'video_poster.PNG' },
+    chapterFile: { type: String, default: '' },
+    ccFile: { type: String, default: 'chapters.vtt' },
+    modalArray: { type: Array, default () { return [] } }
+  },
+  data() {
+    return {
+      setVolume: 100,
+      oldVolume: 100,
+      isMuted: false,
+      isPaused: true,
+      ready: false,
+      PlayTime: 0,
+      CCactive: false,
+      videoUrl: require('~/assets/' + this.$i18n.locale + '/' + this.videoFile),
+      posterUrl: require('~/assets/' + this.$i18n.locale + '/' + this.posterFile),
+      ccUrl: require('~/assets/' + this.$i18n.locale + '/' + this.ccFile)
+    }
+  },
+  computed: {
+    chapterUrl() {
+      if (this.chapterFile) {
+        return require('~/assets/' + this.$i18n.locale + '/' + this.chapterFile)
+      }
+    }
+  },
+  watch: {
+    isMuted() {
+      const video = this.$refs.videoplayer
+      if (this.isMuted) {
+        this.oldVolume = this.setVolume
+        this.setVolume = 0
+        video.muted = true
+
+      } else {
+        video.muted = false
+        this.setVolume = this.oldVolume
+        video.volume = this.setVolume / 100
+      }
+    },
+    setVolume(volume) {
+      const video = this.$refs.videoplayer
+      if (volume == 0) {
+        this.isMuted = true
+      } else {
+        this.isMuted = false
+        video.volume = (volume / 100)
+        this.oldVolume = this.setVolume
+      }
+    }
+  },
+  methods: {
+    showCC() {
+      this.CCactive = !this.CCactive
+    },
+    stopVideo() {
+      const video = this.$refs.videoplayer
+
+    },
+
+    setTime(e) {
+      const video = this.$refs.videoplayer
+      var pos = (e.pageX - (this.$refs.progress.getBoundingClientRect().left)) / this.$refs.progress.offsetWidth;
+      video.currentTime = pos * video.duration;
+
+    },
+    isReady() { this.ready = true },
+    setPlaying() {
+      this.isPaused = !this.isPaused
+      if (!this.isPaused) {
+        this.$refs.videoplayer.play()
+      } else {
+        this.$refs.videoplayer.pause()
+      }
+    },
+    lookPlayTime() {
+      let currentTime = this.$refs.videoplayer.currentTime
+      let duration = this.$refs.videoplayer.duration
+      this.PlayTime = (currentTime / duration) * 100
+    }
+  }
 }
 
+</script>
+<i18n>
+  {
+  "en":{
+  "play":"Play",
+  "pause":"Pause",
+  "closedcaptionning":"Closed Captions"
+  },
+  "fr":{
+  "play":"Jouer",
+  "pause":"Pause",
+  "closedcaptionning":"Sous-titrage codé"
+  }
+  }
+</i18n>
+<style type="text/css" scoped>
+.expand-enter-active,
+.expand-leave-active {
+  transition: all 1s ease;
+  padding: .5em;
+  overflow: hidden;
+}
+
+/* .expand-enter defines the starting state for entering */
+/* .expand-leave defines the ending state for leaving */
+.expand-enter,
+.expand-leave-to {
+  height: 0;
+  padding: 0 10px;
+  opacity: 0;
+}
+
+.CC {
+  height: 4em;
+  padding: .5em;
+  background-color: #000;
+  color: #FFF;
+  font-family: sans-serif;
+  text-align: center;
+  width: 100%;
+
+}
 
 input[type=range] {
   -webkit-appearance: none;
@@ -62,107 +163,108 @@ input[type=range] {
 }
 
 input[type=range]:focus {
-    outline-offset: 0
+  outline-offset: 0
 }
 
 input[type=range]::-webkit-slider-runnable-track {
-    background: #aaa;
-    height: 4px
+  background: #aaa;
+  height: 4px
 }
 
 input[type=range]::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    background: #fff;
-    border: 1px solid #707070;
-    -webkit-box-sizing: content-box;
-    box-sizing: content-box;
-    height: 1.3em;
-    margin-top: -9px;
-    width: 10px
+  -webkit-appearance: none;
+  background: #fff;
+  border: 1px solid #707070;
+  -webkit-box-sizing: content-box;
+  box-sizing: content-box;
+  height: 1.3em;
+  margin-top: -9px;
+  width: 10px
 }
 
 input[type=range]::-moz-range-track {
-    background: #aaa;
-    border: 0
+  background: #aaa;
+  border: 0
 }
 
 input[type=range]::-moz-range-thumb {
-    background: #fff;
-    border: 1px solid #707070;
-    border-radius: 0;
-    height: 1.3em;
-    width: 10px
+  background: #fff;
+  border: 1px solid #707070;
+  border-radius: 0;
+  height: 1.3em;
+  width: 10px
 }
 
 input[type=range]::-ms-track {
-    border: 0;
-    color: transparent;
-    height: 4px
+  border: 0;
+  color: transparent;
+  height: 4px
 }
 
 input[type=range]::-ms-fill-upper {
-    background: #aaa
+  background: #aaa
 }
 
 input[type=range]::-ms-fill-lower {
-    background: #aaa
+  background: #aaa
 }
 
 input[type=range]::-ms-thumb {
-    background: #fff;
-    border: 1px solid #707070;
-    height: 1.3em;
-    width: 10px
+  background: #fff;
+  border: 1px solid #707070;
+  height: 1.3em;
+  width: 10px
 }
 
 .fd-slider {
-    display: inline-block;
-    height: 100%;
-    margin-top: 10px;
-    width: 7em
+  display: inline-block;
+  height: 100%;
+  margin-top: 10px;
+  width: 7em
 }
 
 .fd-slider-handle {
-    background: #fff;
-    border: 1px solid #707070;
-    -webkit-box-sizing: content-box;
-    box-sizing: content-box;
-    width: 10px
+  background: #fff;
+  border: 1px solid #707070;
+  -webkit-box-sizing: content-box;
+  box-sizing: content-box;
+  width: 10px
 }
 
 progress {
-    -webkit-appearance: none;
-    background: #fff;
-    background-clip: padding-box;
-    border: 6px solid #4d4d4d;
-    border-radius: 14px;
-    color: #176ca7;
-    display: block;
-    height: 30px;
-    left: 0;
-    padding: 2px;
-    position: absolute;
-    top: 0;
-    width: 100%
+  -webkit-appearance: none;
+  background: #fff;
+  background-clip: padding-box;
+  border: 6px solid #4d4d4d;
+  border-radius: 14px;
+  color: #587C84;
+  display: block;
+  height: 30px;
+  left: 0;
+  padding: 2px;
+  position: relative;
+  clear: both;
+  top: 0;
+  width: 100%
 }
 
 
 progress::-webkit-progress-bar {
-    background: #fff
+  background: #fff
 }
 
 progress::-webkit-progress-value {
-    background: #176ca7;
-    border-radius: 7px
+  background: #587C84;
+  border-radius: 7px
 }
 
 progress::-moz-progress-bar {
-    background: #176ca7;
-    border-radius: 7px
+  background: #587C84;
+  border-radius: 7px
 }
 
 .progress {
-    height: 22px
+  height: 22px
 }
 
 
@@ -190,6 +292,10 @@ button {
   height: 100%;
   width: 50px;
   cursor: pointer;
+}
+
+input[type=range] {
+  float: left;
 }
 
 button:hover,
--- pages/test.vue
+++ pages/test.vue
@@ -1,33 +1,18 @@
 <template>
 	<div>
-  <i18n-n :value="200"></i18n-n>
-  <i18n-n :value="300" format="currency"></i18n-n>
-  <i18n-n :value="100" format="currency" locale="ja-JP"></i18n-n>
-  <p>{{$n(200,'currency','ca-FR')}}</p>
-  <p>{{ $n(100, 'currency') }}</p>
-  <p>{{ $n(100, 'currency', 'ja-JP') }}</p>
+    <videoPlayer />
+  
   <p>{{users}}</p>
 </div>
 </template>
 <script type="text/javascript">
-	const numberFormats = {
-  'en-US': {
-    currency: {
-      style: 'currency', currency: 'USD'
-    }
-  },
-  'ca-FR': {
-    currency: {
-      style: 'currency', currency: 'CAD', currencyDisplay: 'symbol'
-    }
-  }
-}
+	import videoPlayer from "~/components/interface/videoPlayer"
 export default{
   computed:{
-    users(){
-  return this.$store.state.scorm.name
-    }
-
+    users(){  return this.$store.state.scorm.name    }
+  },
+  components:{
+  	videoPlayer
   }
 }
 </script>

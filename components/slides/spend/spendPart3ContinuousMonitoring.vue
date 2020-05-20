4c126aba73442e941640d64b1275195b6f8841e6
Benoit Lord <BLord@csps-efpc.com>
2020-05-20 14:51:51 -0400

    #835: Changed multiplication, division and substraction symbols to html entities (&times;, &divide; and &minus;)

--- components/slides/spend/spendPart3ContinuousMonitoring.vue
+++ components/slides/spend/spendPart3ContinuousMonitoring.vue
@@ -60,9 +60,9 @@ export default {
     "q2": {
       "text": "The start date for your new employee has been pushed back a month. So they will only be working the remaining six months of the fiscal year instead of seven months. Their salary is $70,000. What do you do?",
       "options": {
-        "1": "Reduce the actual expenditures by $5,833 ($70,000 ÷ 12 months  X 1 month).",
-        "2": "Reduce commitment expenditures by $40,833 ($70,000 ÷ 12 months X 7 months).",
-        "3": "Reduce annual forecast expenditures by $5,833 ($70,000 ÷ 12 months X 1 month)."
+        "1": "Reduce the actual expenditures by $5,833 ($70,000 &divide; 12 months &times; 1 month).",
+        "2": "Reduce commitment expenditures by $40,833 ($70,000 &divide; 12 months &times; 7 months).",
+        "3": "Reduce annual forecast expenditures by $5,833 ($70,000 &divide; 12 months &times; 1 month)."
       },
       "feedback": {
         "1": "<span class='v-wrong' /> <strong>Incorrect</strong>. Since the employee has not yet started, the expenditures cannot be reduced. ",
@@ -74,7 +74,7 @@ export default {
   },
   "fr": {
     "q1": {
-      "text": "Le sous-ministre adjoint a approuvé une demande de voyage de 5&nbsp;000&nbsp;$. Elle a été enregistrée dans le système financier. Une semaine plus tard, au moment de réserver le vol, le prix est passé de 600 $ à 650 $. Que feriez-vous?",
+      "text": "Le sous-ministre adjoint a approuvé une demande de voyage de 5&nbsp;000&nbsp;$. Elle a été enregistrée dans le système financier. Une semaine plus tard, au moment de réserver le vol, le prix est passé de 600&nbsp;$ à 650&nbsp;$. Que feriez-vous?",
       "options": {
         "1": "Modifiez le montant réel de vos dépenses.",
         "2": "Modifiez vos engagements sans demander une augmentation de l’autorisation de déplacement.",
@@ -90,9 +90,9 @@ export default {
     "q2": {
       "text": "La date d’entrée en fonction de votre nouvel employé a été repoussée d’un mois. Il ne travaillera donc que les six mois restants de l’exercice au lieu de sept mois. Son salaire est de 70&nbsp;000&nbsp;$. Qu’est-ce que vous faites?",
       "options": {
-        "1": "Réduire les dépenses réelles de 5&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ ÷ 12 mois X 1 mois).",
-        "2": "Réduire les dépenses d’engagement de 40&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ ÷ 12 mois X 7 mois).",
-        "3": "Réduire les dépenses annuelles prévues de 5&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ ÷ 12 mois X 1 mois)."
+        "1": "Réduire les dépenses réelles de 5&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ &divide; 12 mois &times; 1 mois).",
+        "2": "Réduire les dépenses d’engagement de 40&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ &divide; 12 mois &times; 7 mois).",
+        "3": "Réduire les dépenses annuelles prévues de 5&nbsp;833&nbsp;$ (70&nbsp;000&nbsp;$ &&divide;divide; 12 mois &times; 1 mois)."
       },
       "feedback": {
         "1": "<span class='v-wrong' /> <strong>Incorrect</strong>. Étant donné que l’employé n’a pas encore commencé, les dépenses ne peuvent pas être réduites.",

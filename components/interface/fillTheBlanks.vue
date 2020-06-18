4aa7ca959c7a086d412a7f1ea9019f7b28cca672
csabourin <christian.sabourin@canada.ca>
2020-06-17 22:00:48 -0400

    Attempt at making componet for fill in the blanks

---
+++ components/interface/fillTheBlanks.vue
@@ -0,0 +1,56 @@
+<template>
+  <span>
+    <label class="v-inv" for="q1"> {{$t('label')}}</label>
+    <select v-model="Q1" @change="$emit('selected',Q1)" id="q1">
+      <option disabled value=''>{{$t('qDisabled')}}</option>
+      <option v-for="(term,index) in termList" :key="index" :value="index" v-html="term" />
+    </select>
+    <p>
+      <b-button @click="q1Submit=true" :disabled="!Q1 || q1Submit">{{$t('submit')}}</b-button>
+    </p>
+    <p v-if="Q1">
+      <span class="v-right" v-if="Q1=='1' && q1Submit"><strong>Correct!</strong></span>
+      <span v-else-if="q1Submit" class="v-wrong"><strong>Incorrect.</strong></span></p>
+  </span>
+</template>
+<script type="text/javascript">
+export default {
+  data() {
+    return {
+      Q1: 0,
+      q1Submit: true
+    }
+  },
+  props: {
+    sentence: {
+      type: String,
+      default: `Empty {0}{1} and {2}{3} here`
+    },
+    termList: {
+      type: Object,
+      default: () => { return { "0": "Empty" } }
+    }
+  },
+  i18n: {
+    messages: {
+      en: { passed: "sentence {0}{1}" },
+      fr: { passed: "phrase {0}{1}" }
+    }
+  }
+}
+
+</script>
+<i18n>
+  {
+  "en": {
+  "qDisabled": "Choose",
+  "label": "Select an option to complete the sentence",
+  "sentence":" Empty {0}{1} sentence"
+  },
+  "fr": {
+  "qDisabled": "Choisissez",
+  "label": "Sélectionnez une option pour compléter la phrase",
+  "sentence":"{0}{1} Phrase vide"
+  }
+  }
+</i18n>
---
+++ pages/testComp.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Test Page</h1>
+    <i18n path="q1.sentence">
+      <fillTheBlanks :termList="$t('q1.choices')" />
+      <fillTheBlanks :termList="$t('q1.choices')" />
+    </i18n>
+    <hr>
+  </div>
+</template>
+<script type="text/javascript">
+import fillTheBlanks from "~/components/interface/fillTheBlanks"
+export default {
+  components: {
+    fillTheBlanks
+  }
+}
+
+</script>
+<i18n>
+	{
+    "en":
+    {
+        "q1":
+        {
+            "sentence": "The train is {0} when the moon is {1}",
+            "choices":
+            {
+                "1": "Operating &amp; Maintenance (O&amp;M)",
+                "2": "Salary",
+                "3": "Grants and Contributions (Gs&amp;Cs)",
+                "4": "Capital"
+            },
+            "feedback":
+            {
+                "wrong": "Incorrect",
+                "right": "Correct"
+            }
+        }
+    },
+    "fr":
+    {
+        "q1":
+        {
+            "sentence": "Le train est {0} quand la lune est {1}",
+            "choices":
+            {
+                "1": "Fonctionnement et entretien (F et E)",
+                "2": "Salaires",
+                "3": "Subventions et contributions (S et C)",
+                "4": "Immobilisations"
+            },
+            "feedback":
+            {
+                "wrong": "Incorrect",
+                "right": "Correct"
+            }
+        }
+    }
+}
+</i18n>

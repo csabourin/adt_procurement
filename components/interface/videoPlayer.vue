9e372a6196ec8f38bf8a5ace5641b29ee726388f
Benoit Lord <benoit.lord@canada.ca>
2020-06-29 15:19:59 -0400

    Fixed error in video player when switching language (isInArray function)

--- components/interface/videoPlayer.vue
+++ components/interface/videoPlayer.vue
@@ -489,9 +489,11 @@ export default {
       }
     },
     isInArray(val, arr){
-      for (var i = 0; i < arr.length; i++){
-        if(arr[i] == val){
-          return true;
+      if(arr){
+        for (var i = 0; i < arr.length; i++){
+          if(arr[i] == val){
+            return true;
+          }
         }
       }
       return false;

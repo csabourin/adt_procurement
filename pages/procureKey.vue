8740c3c85069b32d31c0bbd3475c30bdae2c269a
AlexSeguin28 <seguinalexandre@hotmail.ca>
2021-07-08 11:32:01 -0400

    procureKey texts added and bottom nav fixed for Procure module

--- pages/procureKey.vue
+++ pages/procureKey.vue
@@ -3,54 +3,60 @@
     <h2 class="pageTitle">{{$t('KeyMessages')}}</h2>
     <p><img src="~/assets/KeyMessS.svg" alt="" role="presentation" width="250px"></p>
     <p>&nbsp;</p>
-    <p class="quote spend"><q class="bigQuote" :class="{'french-quote':$i18n.locale=='fr'}">{{$t('keyPlanQuote')}}</q><footer>&mdash; Stephen R. Covey</footer></p>
-    <p>&nbsp;</p>
     <b-container style="text-align: left">
       <b-row>
-        <b-col cols="12" lg><span v-if="$i18n.locale=='en'">
-            <h3>Initiate Expenditures and Authorize Commitments</h3>
+        <b-col cols="6" lg><span v-if="$i18n.locale=='en'">
+            <h3>Technical and Bid Requirements and Procurement Strategy</h3>
             <ul>
-              <li>When you decide to obtain goods or services that will result in the eventual spending of funds, such as the decision to hire staff, order supplies, or authorize travel, you are initiating an expenditure.</li>
-              <li>When you commit an expenditure, you are certifying that the money is available. This is known as section 32 of the <em>Financial Administration Act </em> (<abbr>FAA</abbr>).</li>
-              <li>A delegation of financial authority chart shows your organization’s official spending and financial authorities.</li>
-              <li>To learn what you can initiate and commit, consult your own signed delegation form. For those expenditures for which you do not have authority, consult your organization’s delegation of financial authority chart.</li>
+              <li>You identify and define your requirements while taking into account any special considerations or other concerns, such as accessibility, Indigenous procurement and green procurement.</li>
+              <li>Then, you draft your specifications in the form of a statement of work or a statement of requirement with input from stakeholders. Comply with the legal and policy framework to ensure fairness, openness and transparency throughout the procurement process.</li>
+              <li>Finally, you choose a procurement strategy with the help of a procurement specialist. This could be a competitive process, a sole-source contract or a standing offer, for example.</li>
             </ul>
           </span>
           <span v-if="$i18n.locale=='fr'">
-            <p>
-              <h3>Engager et autoriser des dépenses</h3>
+              <h3>Exigences techniques et de soumission et stratégie d’approvisionnement</h3>
               <ul>
-                <li>Lorsque vous décidez d’obtenir des biens ou des services qui entra&icirc;neront une dépense éventuelle, comme l’embauche de personnel, la commande de fournitures ou l’autorisation de voyager, vous engagez une dépense.</li>
-                <li>Lorsque vous engagez une dépense, vous certifiez que l’argent est disponible. C’est ce à quoi on fait référence lorsqu’on parle de l’article 32 de la<em> Loi sur la gestion des finances publiques </em>(<abbr>LGFP</abbr>).</li>
-                <li>Un tableau de délégation des pouvoirs financiers indique les pouvoirs de dépenser et les pouvoirs financiers officiels de votre organisation.</li>
-                <li>Pour savoir ce que vous pouvez engager comme dépense, consultez votre propre carte de délégation signée. Pour les dépenses pour lesquelles vous n’avez pas d’autorisation, consultez le tableau de délégation des pouvoirs financiers de votre organisation.</li>
+                <li>Vous devez relever et définir vos exigences en tenant compte de toute considération particulière ou de toute autre préoccupation, par exemple l’accessibilité, l’approvisionnement auprès d’entreprises autochtones et les achats écologiques.</li>
+                <li>Ensuite, vous devez rédiger vos spécifications sous la forme d’un énoncé de travail ou d’un énoncé des exigences en faisant appel aux intervenants. Respectez le cadre législatif et stratégique pour assurer l’équité, l’ouverture et la transparence de l’ensemble du processus d’approvisionnement.</li>
+                <li>Enfin, vous devez choisir une stratégie d’approvisionnement à l’aide d’un spécialiste en approvisionnement. Vous pourriez par exemple choisir un processus concurrentiel, un contrat à fournisseur unique ou une offre à commandes.</li>
               </ul>
           </span>
         </b-col>
-        <b-col cols="1" class="d-none d-lg-block"></b-col>
-        <b-col cols="12" lg><span v-if="$i18n.locale=='en'">
-            <h3>Exercise Financial Authority</h3>
+        <b-col cols="6" lg><span v-if="$i18n.locale=='en'">
+            <h3>Solicit, Evaluate, Approve and Award Competitive Contracts</h3>
             <ul>
-              <li>When you certify that the work has been performed as required, and services and supplies have been satisfactorily rendered, this is known as section 34.</li>
-              <li>When your financial department ensures that the payment complies with policy requirements, this is section 33.</li>
-            </ul>
-            <h3>Monitor and Control Finances</h3>
-            <ul>
-              <li>To help you track your work plan and budget, you will continually monitor your spending and manage your risks.</li>
+              <li>With the help of a procurement specialist, you prepare the bid solicitation documents and solicit the bid.</li>
+              <li>Once the bidding closes, you evaluate the bids against the established criteria with the help of others and select a bidder.</li>
+              <li>Then, the procurement specialist seeks to have the contract approved by the proper delegated financial and spending (expenditure initiation and transaction) authorities.</li>
+              <li>Finally, once the contract is approved, the procurement specialist awards the contract.</li>
             </ul>
           </span><span v-if="$i18n.locale=='fr'">
-            <p>
-              <h3>Exercer son pouvoir financier</h3>
+              <h3>Solliciter, évaluer, approuver et accorder des contrats concurrentiels</h3>
               <ul>
-                <li>Lorsque vous attestez que les travaux ont été exécutés conformément aux exigences et que les services ont été rendus et les fournitures livrées de façon satisfaisante, il s’agit de l’article 34.</li>
-                <li>Lorsque votre service financier s’assure que le paiement est conforme aux exigences de la politique, il s’agit de l’article 33.</li>
-              </ul>
-              <h3>Surveiller et contr&ocirc;ler les finances</h3>
-              <ul>
-                <li>Pour vous aider à suivre votre plan de travail et votre budget, vous surveillerez continuellement vos dépenses et gérerez vos risques.</li>
+                <li>Avec l’aide d’un spécialiste en approvisionnement, vous devez préparer les documents de demande de soumissions et lancer l’invitation à soumissionner.</li>
+                <li>À la clôture du processus de demande de soumissions, vous devez évaluer les soumissions en fonction des critères établis avec l’aide d’autres personnes, puis sélectionner un soumissionnaire.</li>
+                <li>Ensuite, le spécialiste en approvisionnement fera approuver le contrat par la personne investie des pouvoirs délégués appropriés en matière de finances et de dépenses (pouvoir d’engager des dépenses et d’effectuer des transactions).</li>
+                <li>Enfin, une fois le contrat approuvé, le spécialiste en approvisionnement en fera l’attribution.</li>
               </ul>
           </span></b-col>
-      </b-row>
+          </b-row>
+          <b-row>
+          <b-col cols="12" lg><span v-if="$i18n.locale=='en'">
+            <h3>Administer, Pay and Close-Out Contract</h3>
+            <ul>
+              <li>Once the contract has been awarded, you administer it with the help of the procurement specialist, ensuring that work is performed according to the terms and conditions and records are maintained throughout the process.</li>
+              <li>Once the terms and conditions of the contract are met, you accept the deliverables and the delegated financial authority will perform the certification authority (s. 34 of the Financial Administration Act), which will then proceed to finance to perform the payment authority (s. 33 of the Financial Administration Act) and pay the contractor.</li>
+              <li>Finally, once the contract has been completed, the procurement specialist will close the contract.</li>
+            </ul>
+          </span><span v-if="$i18n.locale=='fr'">
+            <h3>Administration, paiement et clôture du contrat</h3>
+              <ul>
+                <li>Une fois le contrat attribué, vous devez l’administrer avec l’aide du spécialiste en approvisionnement pour vous assurer que les travaux sont accomplis dans le respect des modalités et que des dossiers sont tenus à jour tout au long du processus.</li>
+                <li>Lorsque les modalités du contrat sont remplies, vous devez accepter les produits livrables. La personne investie des pouvoirs financiers délégués doit exercer son pouvoir d’attestation (article 34 de la Loi sur la gestion des finances publiques), après quoi les Finances exerceront leur pouvoir de paiement (article 33 de la Loi sur la gestion des finances publiques) et paieront l’entrepreneur.</li>
+                <li>Finalement, une fois le contrat achevé, le spécialiste en approvisionnement procédera à la clôture du contrat.</li>
+              </ul>
+          </span></b-col>
+          </b-row>
       <b-row>
         <b-col>
           <keyMessagesFiles>
@@ -86,10 +92,10 @@
     </div>
     <div class="bottomNav spendSection" v-else>
       <div class="spendSectionBar"><span>{{$t('spendSectionBar')}}</span></div>
-      <microlearning path="spendPart1" imagePath="InitiateAuthSpending.svg" size="140" time="20" :completion="$store.state.currentPlaying.spendPart1_player" :text="$t('InitiateAuthorizeSpending')" type="video" />
-      <microlearning path="spendPart2" imagePath="ExerciseFinancialAuthority.svg" size="140" time="20" :completion="$store.state.currentPlaying.spendPart2_player" :text="$t('ExerciseFinancialAuthority')" type="video" />
-      <microlearning path="spendPart3" size="140" time="20" :completion="$store.state.currentPlaying.spendPart3_player" imagePath="MonitContFinances.svg" :text="$t('MonitorControlFinances')" type="video" />
-       <microlearning time="5" youAreHere path="spendKey" size="140" :completion="$store.state.currentPlaying.kmSpend" imagePath="KeyMessS.svg" :text="$t('KeyMessages')" type="keyMessages" />
+      <microlearning path="procurePart1" imagePath="InitiateAuthSpending.svg" size="140" time="20" :completion="$store.state.currentPlaying.spendPart1_player" :text="$t('InitiateAuthorizeSpending')" type="video" />
+      <microlearning path="procurePart2" imagePath="ExerciseFinancialAuthority.svg" size="140" time="20" :completion="$store.state.currentPlaying.spendPart2_player" :text="$t('ExerciseFinancialAuthority')" type="video" />
+      <microlearning path="procurePart3" size="140" time="20" :completion="$store.state.currentPlaying.spendPart3_player" imagePath="MonitContFinances.svg" :text="$t('MonitorControlFinances')" type="video" />
+       <microlearning time="5" youAreHere path="procureKey" size="140" :completion="$store.state.currentPlaying.kmSpend" imagePath="KeyMessS.svg" :text="$t('KeyMessages')" type="keyMessages" />
       <microlearning path="exam2" size="140" time="15" imagePath="S-Test.svg" :text="$t('Test')" :completion="parseInt($store.getters['spend/getScore'],10)" :highlighted="chosenScenario == 'justExam'" type="exam" questionNum="20" />
     </div>
   </div>

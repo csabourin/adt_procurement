d45d3754664d4c97ad1925b36b4e58f7cdbce3ee
AlexSeguin28 <seguinalexandre@hotmail.ca>
2021-07-28 16:41:10 -0400

    HTML Jobaids have been created (1 out of 4 have its content)

---
+++ components/jobaids/jobAid_Oversight_EN.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Oversight and Challenge Processes in Procurement</h1>
+        <p>You now know what you can and cannot do in terms of your decisions and actions in the procurement process, but what if you don’t abide by these rules? What are the repercussions?</p>
+        <p>There are different levels of authorities that provide oversight in government procurement to ensure the rules are being followed. Authorities can be internal to organizations and also external to them. For example, many departments may have ethics advisors or compliance and oversight branches and interdepartmental governance committees. These authorities provide support for resolving issues internally before formal escalation may be required.</p>
+        <p>The authorities with oversight responsibilities for procurement in the federal government are listed below. Their roles differ depending on the nature and monetary amount of the procurement.</p>
+        <div>
+            <h2>Office of the Procurement Ombudsman</h2>
+        </div>
+        <p>The <i>Federal Accountability Act</i> amended the <i>Department of Public Works and Government Services Act</i> to provide for the appointment and mandating of a Procurement Ombudsman, who has four primary functions:</p>
+        <table>
+            <tr>1</tr>
+            <tr>Review the practices of organizations for acquiring materiel and services to assess their fairness, openness and transparency. Make any appropriate recommendations to the relevant organization for the improvement of these practices.</tr>
+        </table>
+        <div>
+            <h2>Canadian International Trade Tribunal</h2>
+        </div>
+        <div>
+            <h2>Federal Court</h2>
+        </div>
+        <div>
+            <h2>Internal Audit</h2>
+        </div>
+        <div>
+            <h2>Office of the Comptroller General</h2>
+        </div>
+        <div>
+            <h2>Senior Designated Officials for the Management of Projects and Programs</h2>
+        </div>
+        <div>
+            <h2>Auditor General</h2>
+        </div>
+        <div>
+            <h2>Standing Review Committees</h2>
+        </div>
+    </div>
+</template>

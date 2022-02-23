8f01c46bec5804c9a1c40b9b4ad132744e0f93c2
Segalex <96079629+Segalex@users.noreply.github.com>
2022-02-23 15:45:49 -0500

    Modified stored infos

--- store/currentPlaying.js
+++ store/currentPlaying.js
@@ -13,16 +13,16 @@ export const state = () => ({
   spendPart2: parseInt(window.localStorage.getItem("procurePart2"),10) || 0,
   spendPart3: parseInt(window.localStorage.getItem("procurePart3"),10) || 0,
   reportPart1: parseInt(window.localStorage.getItem("reportProcPart1"),10) || 0,
-  reportPart2: parseInt(window.localStorage.getItem("reportPart2"),10) || 0,
+  reportPart2: parseInt(window.localStorage.getItem("reportProcPart2"),10) || 0,
   manage: parseInt(window.localStorage.getItem("manage"),10) || 0,
-  buildWP_player: parseInt(window.localStorage.getItem("buildWP_player"),10) || 0,
-  createBudget_player: parseInt(window.localStorage.getItem("createBudget_player"),10) || 0,
-  forecast_player: parseInt(window.localStorage.getItem("forecast_player"),10) || 0,
-  spendPart1_player: parseInt(window.localStorage.getItem("spendPart1_player"),10) || 0,
-  spendPart2_player: parseInt(window.localStorage.getItem("spendPart2_player"),10) || 0,
-  spendPart3_player: parseInt(window.localStorage.getItem("spendPart3_player"),10) || 0,
-  reportPart1_player: parseInt(window.localStorage.getItem("reportPart1_player"),10) || 0,
-  reportPart2_player: parseInt(window.localStorage.getItem("reportPart2"),10) || 0,
+  buildWP_player: parseInt(window.localStorage.getItem("planProcPart1_player"),10) || 0,
+  createBudget_player: parseInt(window.localStorage.getItem("planProcPart2_player"),10) || 0,
+  forecast_player: parseInt(window.localStorage.getItem("planProcPart3_player"),10) || 0,
+  spendPart1_player: parseInt(window.localStorage.getItem("procurePart1_player"),10) || 0,
+  spendPart2_player: parseInt(window.localStorage.getItem("procurePart2_player"),10) || 0,
+  spendPart3_player: parseInt(window.localStorage.getItem("procurePart3_player"),10) || 0,
+  reportPart1_player: parseInt(window.localStorage.getItem("reportProcPart1_player"),10) || 0,
+  reportPart2_player: parseInt(window.localStorage.getItem("reportProcPart2_player"),10) || 0,
   manage_player: parseInt(window.localStorage.getItem("manage_player"),10) || 0,
   currentModule: window.localStorage.getItem("currentModule") || "",
   chosenScenario: window.localStorage.getItem("chosenScenario") || "takeCourse",
@@ -83,39 +83,39 @@ export const mutations = {
     state.reportPart1 = playing
   },
   setReportPart2(state, playing) {
-    window.localStorage.setItem("reportPart2", playing)
+    window.localStorage.setItem("reportProcPart2", playing)
     state.reportPart2 = playing
   },
   setBuildWP_player(state, playing) {
-    window.localStorage.setItem("buildWP_player", playing)
+    window.localStorage.setItem("planProcPart1_player", playing)
     state.buildWP_player = playing
   },
   setforecast_player(state, playing) {
-    window.localStorage.setItem("forecast_player", playing)
+    window.localStorage.setItem("planProcPart3_player", playing)
     state.forecast_player = playing
   },
   setCreateBudget_player(state, playing) {
-    window.localStorage.setItem("createBudget_player", playing)
+    window.localStorage.setItem("planProcPart2_player", playing)
     state.createBudget_player = playing
   },
   setSpendPart1_player(state, playing) {
-    window.localStorage.setItem("spendPart1_player", playing)
+    window.localStorage.setItem("procurePart1_player", playing)
     state.spendPart1_player = playing
   },
   setSpendPart2_player(state, playing) {
-    window.localStorage.setItem("spendPart2_player", playing)
+    window.localStorage.setItem("procurePart2_player", playing)
     state.spendPart2_player = playing
   },
   setSpendPart3_player(state, playing) {
-    window.localStorage.setItem("spendPart3_player", playing)
+    window.localStorage.setItem("procurePart3_player", playing)
     state.spendPart3_player = playing
   },
   setReportPart1_player(state, playing) {
-    window.localStorage.setItem("reportPart1", playing)
+    window.localStorage.setItem("reportProcPart1_player", playing)
     state.reportPart1_player = playing
   },
   setReportPart2_player(state, playing) {
-    window.localStorage.setItem("reportPart2", playing)
+    window.localStorage.setItem("reportProcPart2_player", playing)
     state.reportPart2_player = playing
   },
   setmanage_player(state, playing) {

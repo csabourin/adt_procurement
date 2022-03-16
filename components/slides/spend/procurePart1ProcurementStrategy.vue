707f91981297357f933e329885ad89c47d65bfde
Segalex <96079629+Segalex@users.noreply.github.com>
2022-03-16 12:58:15 -0400

    Fixed the broken button in procure mod 4 tile 8

--- components/slides/spend/procurePart1ProcurementStrategy.vue
+++ components/slides/spend/procurePart1ProcurementStrategy.vue
@@ -39,7 +39,7 @@
         <div class="text-center">
           <b-button-group :aria-label="$t('questionNav')" class="mt-2">
             <b-button @click="[tabIndex--, focus()]" :disabled="tabIndex<=0">{{$t('previousPage')}}</b-button>
-            <b-button @click="[tabIndex++, focus()]" :disabled="tabIndex>=6">{{$t('nextPage')}}</b-button>
+            <b-button @click="[tabIndex++, focus()]" :disabled="tabIndex>=8">{{$t('nextPage')}}</b-button>
           </b-button-group>
         </div>
     </b-card>

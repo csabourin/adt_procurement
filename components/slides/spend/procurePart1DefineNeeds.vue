c51d43c63dcec41ff575797cb4620fea24ec8003
Charles Letellier <realcharlesletellier@gmail.com>
2022-11-23 06:33:27 -0500

    fixed quiz and video issue

--- components/slides/spend/procurePart1DefineNeeds.vue
+++ components/slides/spend/procurePart1DefineNeeds.vue
@@ -135,7 +135,7 @@
         <div class="text-center">
           <b-button-group :aria-label="$t('questionNav')" class="mt-2">
             <b-button @click="[tabIndex--, focus()]" :disabled="tabIndex<=0">{{$t('previousPage')}}</b-button>
-            <b-button @click="[tabIndex++, focus()]" :disabled="tabIndex>=1">{{$t('nextPage')}}</b-button>
+            <b-button @click="[tabIndex++, focus()]" :disabled="tabIndex>=3">{{$t('nextPage')}}</b-button>
           </b-button-group>
         </div>
   <hr>
